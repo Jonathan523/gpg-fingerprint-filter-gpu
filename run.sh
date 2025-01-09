101d2813d6d4c98c0a5c2d4443f35530f85fdae4
Jonathan Zhang <admin@cestlavie.moe>
2025-01-10 01:47:29 +0800

    update

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH" >> ~/.bashr
 source ~/.bashrc
 source /etc/ai_booster enable
 echo "Installing dependencies..."
-apt update && apt install -y screen
-pip install pgpy
+DEBIAN_FRONTEND=noninteractive apt update -qq && apt install -y screen -qq
+pip install pgpy -qq
 echo "Downloading dependencies..."
 wget -q https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.9.4.tar.gz 
 wget -q https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.51.tar.gz
@@ -19,10 +19,10 @@ tar zxf libgcrypt-1.9.4.tar.gz
 tar zxf libgpg-error-1.51.tar.gz
 cd libgpg-error-1.51
 echo "Building and Installing libgpg-error..."
-./configure && make -j$(nproc) && make install
+./configure > /dev/null 2>&1 && make -j$(nproc) > /dev/null 2>&1 && make install > /dev/null 2>&1 
 cd ../libgcrypt-1.9.4
 echo "Building and Installing libgcrypt..."
-./configure && make -j$(nproc) && make install
+./configure > /dev/null 2>&1 && make -j$(nproc) > /dev/null 2>&1 && make install > /dev/null 2>&1 
 cd ..
 echo "Building gpg-fingerprint-filter-gpu..."
 make -j$(nproc)
@@ -33,7 +33,7 @@ for ((i=1; i<=4; i++)); do
     TASK="./gpg-fingerprint-filter-gpu -j 34 -a ed25519 -t 59654321 -m Y \"x{10}|x{11}|x{12}|x{13}|x{14}|x{15}|x{16}|x{4}y{8}\" /root/fs/output.pgp"
     screen -dmS "$SESSION_NAME"
     screen -S "$SESSION_NAME" -X stuff "$TASK\n"
-    echo "Started task $TASK in screen session $SESSION_NAME"
+    echo "Started task in screen session $SESSION_NAME"
 done
 sleep 3
 TASK="python $(pwd)/ca.py /root/fs/output.pgp/ /root/fs/dist/ >> /root/fs/log.txt"
